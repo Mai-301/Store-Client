fbef3b5f6c1216cbf96e222a7997308b69f61033
Mai-301 <m.m.sadek.301@hotmail.com>
2018-04-06 21:39:40 +0200

    Implementing the controller

---
+++ api/controllers/storeController.js
@@ -0,0 +1,38 @@
+'use strict';
+var mongoose = require('mongoose'),
+    Store = mongoose.model('Stores');
+
+exports.list_all_stores = function (req, res) {
+    Store.find({}, function (err, store) {
+        if (err)
+            res.send(err);
+        res.json(store);
+    });
+};
+
+exports.create_a_store = function (req, res) {
+    var new_store = new Store(req.body);
+    new_store.save(function (err, store) {
+        if (err)
+            res.send(err);
+        res.json(store);
+    });
+};
+
+exports.read_a_store = function (req, res) {
+    Store.findById(req.params.storeId, function (err, store) {
+        if (err)
+            res.send(err);
+        res.json(store);
+    });
+};
+
+exports.delete_a_store = function (req, res) {
+    Store.remove({
+        _id: req.params.storeId
+    }, function (err, store) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'Store deleted successfully' });
+    });
+};
--- api/models/storeModel.js
+++ api/models/storeModel.js
@@ -1,13 +1,14 @@
+'use strict';
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var StoreSchema = new Schema({
-  key: {
-    type: String,
-  },
-  value: {
-    type: String,
-  },
+    key: {
+        type: String,
+    },
+    value: {
+        type: String,
+    },
 });
 
 module.exports = mongoose.model('Store', StoreSchema);
